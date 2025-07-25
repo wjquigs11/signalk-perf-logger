80ccb7a817f2c2daaecce932e981a905f5dad2af
benoit-jpi <jeanpierre.f.benoit@gmail.com>
2025-07-25 17:14:14 +0200

    Update mineur

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const debug = require("debug")("signalk:signalk-perf-logger");
-const util = require("util");
-const _ = require('lodash')
-const path = require('path')
-const fs = require('fs')
-const { spawn } = require('child_process')
+/****************************************************************************
+ISC License
 
-/*
+Copyright (c) 2025 Jean-Pierre Benoit
 
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+const debug = require("debug")("signalk:signalk-engine-state");
+
+*****************************************************************************
 Signal K server plugin to log performance data to csv files.
 
 Features:
@@ -16,7 +26,13 @@ Features:
 
 TODO:
 
-*/
+*****************************************************************************/
+const debug = require("debug")("signalk:signalk-perf-logger");
+const util = require("util");
+const _ = require('lodash')
+const path = require('path')
+const fs = require('fs')
+const { spawn } = require('child_process')
 
 module.exports = function(app) {
     var plugin = {};
